ae61b7adc697f7724df2585424981ef218e2d3bc
NonoElemento <israel.ipnet@gmail.com>
2021-03-06 09:10:30 -0300

    06-03-2021

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
 <!-- Custom CSS -->
 <link href="assets/css/styles.css" rel="stylesheet">
 <link href="assets/css/font-awesome.min.css" rel="stylesheet">
-<link href="assets/css/animate-custom.css" rel="stylesheet"> 
+<link href="assets/css/animate-custom.css" rel="stylesheet">
 <script src="assets/js/jquery.min.js"></script>
 <script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
 
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --> 
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -25,11 +25,11 @@
 
 </head>
 <body data-target="#navbar-main">
-<div id="navbar-main"> 
+<div id="navbar-main">
   <!-- Fixed navbar -->
   <div class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
-	 
+
       <div class="navbar-header">
 	   <div class="pull-left logo">
         <a class="" href="#home"><img src="assets/img/logo.png" /></a>
@@ -39,14 +39,14 @@
       <div class="navbar-collapse collapse">
         <ul class="nav navbar-nav pull-right">
           <li><a href="#home-section" class="smoothScroll">Início</a></li>
-          <li> <a href="#services" class="smoothScroll">Sobre Nós</a></li>		  
-          <li> <a href="#about" class="smoothScroll"> Nossas Ofertas</a></li>
+          <li> <a href="#services" class="smoothScroll">Sobre Nós</a></li>
+          <li> <a href="#portfolio" class="smoothScroll"> Nossas Ofertas</a></li>
           <!--<li> <a href="#portfolio" class="smoothScroll"> Special</a></li>
           <li> <a href="#team" class="smoothScroll"> Chef Team</a></li>-->
           <li> <a href="#contact" class="smoothScroll"> Em Contato</a></li>
         </ul>
       </div>
-      <!--/.nav-collapse --> 
+      <!--/.nav-collapse -->
     </div>
   </div>
 </div>
@@ -54,19 +54,19 @@
 <!-- ==== HEADERWRAP ==== -->
 <div id="home-section">
 <div id="headerwrap" name="home">
-  <header class="clearfix tb"> 
+  <header class="clearfix tb">
  <div class="tb-cell text-center">
  <i class="fa fa-heart-o icon"></i>
     <h1>loucos por café</h1>
     <p>Origem e certeza do bom café</p>
-    <a href="#portfolio" class="smoothScroll btn btn-lg">Veja lotes disponíveis</a> 
+    <a href="#portfolio" class="smoothScroll btn btn-lg">Veja lotes disponíveis</a>
 	</div>
 	</header>
 </div>
 
 </div>
 <div class="div-pattern2"></div>
-<!-- /headerwrap --> 
+<!-- /headerwrap -->
 
 
 
@@ -74,191 +74,73 @@
 <div id="services" name="services">
   <div class="container">
     <div class="row">
-      <h2 class="centered">WHAT WE OFFER</h2>
+      <h2 class="centered">Sobre Nós</h2>
       <hr>
       <div class="col-lg-8 col-lg-offset-2">
         <p class="large">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
       </div>
       <div class="col-lg-4 callout"> <i class="fa fa-cubes fa-3x"></i>
-        <h3>COFFEE</h3>
+        <h3>SOLIDEZ</h3>
         <p>Erat imperdiet dissentias ea usu, alia aliquid corrumpit ea qui. Eu vim oratio conclusionemque, vel at errem nominavi delicatissimi.</p>
       </div>
       <div class="col-lg-4 callout"> <i class="fa fa-flask fa-3x"></i>
-        <h3>TEA</h3>
+        <h3>CONHECIMENTO</h3>
         <p>Erat imperdiet dissentias ea usu, alia aliquid corrumpit ea qui. Eu vim oratio conclusionemque, vel at errem nominavi delicatissimi.</p>
       </div>
       <div class="col-lg-4 callout"> <i class="fa fa-life-ring fa-3x"></i>
-        <h3>CAKES</h3>
+        <h3>MERCADO</h3>
         <p>Erat imperdiet dissentias ea usu, alia aliquid corrumpit ea qui. Eu vim oratio conclusionemque, vel at errem nominavi delicatissimi.</p>
       </div>
     </div>
-    <!-- row --> 
+    <!-- row -->
   </div>
 </div>
-<!-- container --> 
-<section class="hero-section section">  
+<!-- container -->
+<section class="hero-section section">
         <div class="container">
             <div class="highlight tb">
                 <div class="tb-cell">
-                    <p>Erat imperdiet dissentias ea usu, alia aliquid corrumpit ea qui!</br>
-					erant bonorum albucius et per, ei sapientem accommodare est. Saepe dolorum constituam ei!!</p>
+                    <p>FAÇA JÁ SUA RESERVA COM BASE EM NOSSAS OFERTAS. OS MELHORES LOTES, PREPARADOS E SEPARADOS POR QUEM ENTENDE DE CAFÉ!</br>
+					A certeza de um bom negócio, com a qualidade que você deseja.</p>
                 </div>
                 <div class="links tb-cell">
                     <div class="reservation-link">
-                        <a href="#" class="btn reservation-btn">FAÇA SUA RESERVA</a> 
-                    </div> 
+                        <a href="#PORTFOLIO" class="btn reservation-btn">FAÇA SUA RESERVA</a>
+                    </div>
                 </div>
             </div>
         </div>
-		
+
     </section>
 	<div class="div-pattern"></div>
-<!-- ==== ABOUT ==== -->
-<div id="about" name="about">
-  <div class="container">
-    <div class="row white">
-      <h2 class="centered">Our Shop</h2>
-      <hr>
-      <div class="col-md-6"> <img class="img-responsive" src="assets/img/about/about1.jpg" align=""> </div>
-      <div class="col-md-6">
-        <h3>Who we are</h3>
-        <p>Lorem ipsum dolor sit amet, quo meis audire placerat eu, te eos porro veniam. An everti maiorum detracto mea. Eu eos dicam voluptaria, erant bonorum albucius et per, ei sapientem accommodare est. Saepe dolorum constituam ei vel. Te sit malorum ceteros repudiandae, ne tritani adipisci vis.</p>
-        <h3>Why choose us?</h3>
-        <p>Lorem ipsum dolor sit amet, quo meis audire placerat eu, te eos porro veniam. An everti maiorum detracto mea. Eu eos dicam voluptaria, erant bonorum albucius et per, ei sapientem accommodare est. Saepe dolorum constituam ei vel.</p>
-      </div>
-    </div>
-    <!-- row --> 
-  </div>
-</div>
-<!-- container --> 
-<!-- ==== PORTFOLIO ==== 
+<!-- container -->
+<!-- ==== PORTFOLIO ==== -->
 <div id="portfolio" name="portfolio">
   <div class="container">
     <div class="row">
-      <h2 class="centered">What Special</h2>
+      <h2 class="centered">Nossas Ofertas</h2>
       <hr>
       <div class="col-lg-8 col-lg-offset-2 centered">
-        <p class="large">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+        <p class="large">A oportunidade de melhores preços, o atestado de procedência, a certeza do melhor café</p>
       </div>
     </div>
-    <!-- /row 
+    <!-- /row -->
     <div class="container">
-      <div class="row"> 
-        
-        <!-- PORTFOLIO IMAGE 1 
-        <div class="col-md-4 ">
-          <div class="grid overlay">
-            <figure> <img class="img-responsive" src="assets/img/portfolio/folio01.jpg" alt="">
-              <figcaption>
-                <h5>Coco Cake</h5>
-                <a data-toggle="modal" href="#myModal" class="btn btn-default">More Details</a> </figcaption>
-              <!-- /figcaption -
-            </figure>
-            <!-- /figure 
-          </div>
-          <!-- /grid-overlay  
-        </div> 
-        
-        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Rose Cake</h4>
-              </div>
-              <div class="modal-body">
-                <p><img class="img-responsive" src="assets/img/portfolio/folio01-preview.jpg" alt=""></p>
-                <p>Lorem ipsum dolor sit amet, quo meis audire placerat eu, te eos porro veniam. An everti maiorum detracto mea. </p> 
-              </div> 
-            </div>
-            <!-- /.modal-content  
-          </div>
-          <!-- /.modal-dialog 
-        </div>
-        <!-- /.modal --> 
-        
-        <!-- PORTFOLIO IMAGE 2 
-        <div class="col-md-4">
-          <div class="grid overlay">
-            <figure> <img class="img-responsive" src="assets/img/portfolio/folio02.jpg" alt="">
-              <figcaption>
-                <h5>Green Tea</h5>
-                <a data-toggle="modal" href="#myModal" class="btn btn-default">More Details</a> </figcaption>
-              <!-- /figcaption  
-            </figure>
-            <!-- /figure  
-          </div>
-          <!-- /grid-overlay  
-        </div>
-        
-        <!-- PORTFOLIO IMAGE 3 
-        <div class="col-md-4">
-          <div class="grid overlay">
-            <figure> <img class="img-responsive" src="assets/img/portfolio/folio03.jpg" alt="">
-              <figcaption>
-                <h5>Cappuccino</h5>
-                <a data-toggle="modal" href="#myModal" class="btn btn-default">More Details</a> </figcaption>
-              <!-- /figcaption  
-            </figure>
-            <!-- /figure 
-          </div>
-          <!-- /grid-overlay  
-        </div>
-      </div>
-      <!-- /row --> 
-      
-      <!-- PORTFOLIO IMAGE 4 
       <div class="row">
-        <div class="col-md-4 ">
-          <div class="grid overlay">
-            <figure> <img class="img-responsive" src="assets/img/portfolio/folio04.jpg" alt="">
-              <figcaption>
-                <h5>Green Tea</h5>
-                <a data-toggle="modal" href="#myModal" class="btn btn-default">More Details</a> </figcaption>
-              <!-- /figcaption  
-            </figure>
-            <!-- /figure  
-          </div>
-          <!-- /grid-overlay  
-        </div>
-        
-        <!-- PORTFOLIO IMAGE 5 
-        <div class="col-md-4">
-          <div class="grid overlay">
-            <figure> <img class="img-responsive" src="assets/img/portfolio/folio05.jpg" alt="">
-              <figcaption>
-                <h5>Cookies</h5>
-                <a data-toggle="modal" href="#myModal" class="btn btn-default">More Details</a> </figcaption>
-              <!-- /figcaption  
-            </figure>
-            <!-- /figure 
-          </div>
-          <!-- /grid-overlay  
-        </div>
-        
-        <!-- PORTFOLIO IMAGE 6 
-        <div class="col-md-4">
-          <div class="grid overlay">
-            <figure> <img class="img-responsive" src="assets/img/portfolio/folio06.jpg" alt="">
-              <figcaption>
-                <h5>Cake</h5>
-                <a data-toggle="modal" href="#myModal" class="btn btn-default">More Details</a> </figcaption>
-              <!-- /figcaption  
-            </figure>
-            <!-- /figure  
-          </div>
-          <!-- /grid-overlay  
-        </div>
-        <!-- /col  
+        <!-- Carrega os lotes em atividade -->
+        <?php include "lotes.php";?>
+
+        <!-- /col -->
       </div>
-      <!-- /row  
+      <!-- /row -->
     </div>
-    <!-- /row  
+    <!-- /row -->
   </div>
 </div>
-<!-- /container --> 
+<!-- /container -->
 
-<!-- ==== TEAM MEMBERS ==== 
+
+<!-- ==== TEAM MEMBERS ====
 <div id="team" name="team">
   <div class="container">
     <div class="row centered">
@@ -284,12 +166,12 @@
         <h4><b>Shannon</b></h4>
         <p>Consectetuer eu nam. Saepe legendos vulputate eu quo, id mea comprehensam signifer.</p>
         <a href="#"><i class="fa fa-twitter"></i></a> <a href="#"><i class="fa fa-facebook"></i></a> <a href="#"><i class="fa fa-linkedin"></i></a> </div>
-     
+
     </div>
   </div>
-  <!-- row  
+  <!-- row
 </div>
-<!-- container --> 
+<!-- container -->
 
 <!-- ==== CONTACT ==== -->
 <div id="contact" name="contact">
@@ -297,7 +179,7 @@
     <div class="row">
       <h2 class="centered">Em contato</h2>
       <hr>
-    
+
     </div>
     <div class="row">
       <div class="col-md-7">
@@ -313,36 +195,36 @@
             </div>
             <div class="col-md-12 form-group">
               <input class="form-control" id="zap" name="zap" placeholder="Telefone Whatsapp" type="text" required />
-            </div>			
+            </div>
 			 <div class="col-md-12 form-group">
-              <textarea class="form-control" id="message" name="message" placeholder="Mensagem" rows="5"></textarea>             
+              <textarea class="form-control" id="message" name="message" placeholder="Mensagem" rows="5"></textarea>
               <button class="btn btn-lg pull-left" type="submit">Enviar mensagem</button>
             </div>
           </div>
-           
+
         </form>
-        <!-- form --> 
+        <!-- form -->
       </div>
 	  <div class="col-md-5">
-	         
-       
+
+
 	  <i class="fa fa-map-marker fa-2x"></i>
-        <strong>Coffee Shop</strong>
-		<p>321 Awesome Street<br>
-          New York, NY 12345</p>
-      
-      <br/> 
+        <strong>Escritório</strong>
+		<p>Barão do Rio Branco, 999<br>
+          Manhuaçu/MG</p>
+
+      <br/>
 	  <i class="fa fa-envelope-o fa-2x"></i>
         <p>email</p>
-       
-       <br/> 
+
+       <br/>
 	  <i class="fa fa-phone fa-2x"></i>
         <p> telefone</p>
-       
+
 	  </div>
     </div>
-    <!-- row --> 
-    
+    <!-- row -->
+
   </div>
 </div>
 <!-- container -->
@@ -364,12 +246,12 @@
 </div>
 
 <!-- Bootstrap core JavaScript
-    ================================================== --> 
-<!-- Placed at the end of the document so the pages load faster --> 
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
 
-<script type="text/javascript" src="assets/js/bootstrap.min.js"></script> 
-<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script> 
-<script type="text/javascript" src="assets/js/smoothscroll.js"></script> 
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="assets/js/smoothscroll.js"></script>
 <script type="text/javascript" src="assets/js/custom-scripts.js"></script>
 </body>
 </html>
---
+++ lotes.php
@@ -0,0 +1,104 @@
+<!-- PORTFOLIO IMAGE 1 -->
+<div class="col-md-4 ">
+  <div class="grid overlay">
+    <figure> <img class="img-responsive" src="assets/img/portfolio/folio01.jpg" alt="">
+      <figcaption>
+        <h5>Arábica - Tipo 2</h5>
+        <a data-toggle="modal" href="#myModal" class="btn btn-default">Reserve</a> </figcaption>
+      <!-- /figcaption -->
+    </figure>
+    <!-- /figure -->
+  </div>
+  <!-- /grid-overlay -->
+</div>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Arábica</h4>
+      </div>
+      <div class="modal-body">
+        <p><img class="img-responsive" src="assets/img/portfolio/folio01-preview.jpg" alt=""></p>
+        <p>Lorem ipsum dolor sit amet, quo meis audire placerat eu, te eos porro veniam. An everti maiorum detracto mea. </p>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
+<!-- PORTFOLIO IMAGE 2 -->
+<div class="col-md-4">
+  <div class="grid overlay">
+    <figure> <img class="img-responsive" src="assets/img/portfolio/folio02.jpg" alt="">
+      <figcaption>
+        <h5>Arábica - Tipo 8</h5>
+        <a data-toggle="modal" href="#myModal" class="btn btn-default">Reserve</a> </figcaption>
+      <!-- /figcaption -->
+    </figure>
+    <!-- /figure -->
+  </div>
+  <!-- /grid-overlay -->
+</div>
+
+<!-- PORTFOLIO IMAGE 3 -->
+<div class="col-md-4">
+  <div class="grid overlay">
+    <figure> <img class="img-responsive" src="assets/img/portfolio/folio03.jpg" alt="">
+      <figcaption>
+        <h5>Conilon - Tipo 5</h5>
+        <a data-toggle="modal" href="#myModal" class="btn btn-default">Reserve</a> </figcaption>
+      <!-- /figcaption -->
+    </figure>
+    <!-- /figure -->
+  </div>
+  <!-- /grid-overlay -->
+</div>
+</div>
+<!-- /row -->
+
+<!-- PORTFOLIO IMAGE 4 -->
+<div class="row">
+<div class="col-md-4 ">
+  <div class="grid overlay">
+    <figure> <img class="img-responsive" src="assets/img/portfolio/folio04.jpg" alt="">
+      <figcaption>
+        <h5>Bourbon - Tipo 3</h5>
+        <a data-toggle="modal" href="#myModal" class="btn btn-default">Reserve</a> </figcaption>
+      <!-- /figcaption -->
+    </figure>
+    <!-- /figure -->
+  </div>
+  <!-- /grid-overlay -->
+</div>
+
+<!-- PORTFOLIO IMAGE 5 -->
+<div class="col-md-4">
+  <div class="grid overlay">
+    <figure> <img class="img-responsive" src="assets/img/portfolio/folio05.jpg" alt="">
+      <figcaption>
+        <h5>Conilon - Tipo 6</h5>
+        <a data-toggle="modal" href="#myModal" class="btn btn-default">Reserve</a> </figcaption>
+      <!-- /figcaption -->
+    </figure>
+    <!-- /figure -->
+  </div>
+  <!-- /grid-overlay -->
+</div>
+
+<!-- PORTFOLIO IMAGE 6 -->
+<div class="col-md-4">
+  <div class="grid overlay">
+    <figure> <img class="img-responsive" src="assets/img/portfolio/folio06.jpg" alt="">
+      <figcaption>
+        <h5>Arábica - Tipo 7</h5>
+        <a data-toggle="modal" href="#myModal" class="btn btn-default">Reserve</a> </figcaption>
+      <!-- /figcaption -->
+    </figure>
+    <!-- /figure -->
+  </div>
+  <!-- /grid-overlay -->
+</div>
